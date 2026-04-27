b3037aefd0222a2e769007b39646e40a3f7f8735
Alex Francis Bannach <leco.sfs@gmail.com>
2026-04-27 08:52:03 -0300

    Commands

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -7,17 +7,76 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   
   })
 
-  it.only('preenche os campos obrigatórios e envia o formulário', () => {
+  it('preenche os campos obrigatórios e envia o formulário', () => {
     const longText = Cypress._.repeat('Lorem ipsum dolor sit amet, consectetur adipiscing elit. ', 10)
     
     cy.get('#firstName').type('João')
     cy.get('#lastName').type('Silva')
     cy.get('#email').type('joao.silva@example.com')
-    cy.get('#open-text-area')
-      .should('be.visible')
-      .type(longText, { delay: 0 })
-    cy.get('button[type="submit"]').click()
+    cy.get('#open-text-area').should('be.visible').type(longText, { delay: 0 })
+    cy.contains('button', 'Enviar').click()
     
     cy.get('.success').should('be.visible')
   })
+
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('João')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('joao.silva@invalid-email')
+    cy.get('#open-text-area').type('Teste de mensagem')
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error').should('be.visible') 
+    })
+
+    it('campo telefone continua vazio quando preenchido com valor não numérico', () => {
+      cy.get('#phone')
+        .type('abcde')
+        .should('have.value', '')
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido', () => {
+      cy.get('#firstName').type('João')
+      cy.get('#lastName').type('Silva')
+      cy.get('#email').type('joao.silva@example.com')
+      cy.get('#open-text-area').type('Teste de mensagem')
+      cy.get('#phone-checkbox').click()
+      cy.contains('button', 'Enviar').click()
+
+      cy.get('.error').should('be.visible')
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+      cy.get('#firstName')
+        .type('João')
+        .should('have.value', 'João')
+        .clear()
+        .should('have.value', '')
+      cy.get('#lastName')
+        .type('Silva')
+        .should('have.value', 'Silva')
+        .clear()
+        .should('have.value', '')
+      cy.get('#email')
+        .type('joao.silva@example.com')
+        .should('have.value', 'joao.silva@example.com')
+        .clear()
+        .should('have.value', '')
+      cy.get('#phone')
+        .type('1234567890')
+        .should('have.value', '1234567890')
+        .clear()
+        .should('have.value', '')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+      cy.contains('button', 'Enviar').click()
+      cy.get('.error').should('be.visible')
+    })
+
+    it('envia o formulário com sucesso usando um comando customizado', () => {
+      cy.fillMandatoryFieldsAndSubmit()
+
+      cy.get('.success').should('be.visible')
+    })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,16 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (data = {
+    firstName: 'Marcos',
+    lastName: 'Antonio',
+    email: 'novo@teste.com',
+    message: Cypress._.repeat('Lorem ipsum dolor sit amet, consectetur adipiscing elit. ', 10),
+    phone: '1234567890'
+}) => {
+  cy.get('#firstName').type(data.firstName)
+  cy.get('#lastName').type(data.lastName)
+  cy.get('#email').type(data.email)
+  cy.get('#open-text-area').should('be.visible').type(data.message, { delay: 0 })
+  cy.get('#phone-checkbox').click()
+  cy.get('#phone').type(data.phone)
+
+  cy.contains('button', 'Enviar').click()
+})
